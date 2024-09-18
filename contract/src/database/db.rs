e4a39e444b5019423d7fb7ede05a77d383e9035c
Abisuwa <ayodejiabisuwa23@gmail.com>
2024-09-18 17:37:16 +0100

    [feat] - rearranged the code structure of the smart contract

---
+++ contract/src/database/db.rs
@@ -0,0 +1,26 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::{LookupMap, UnorderedMap, UnorderedSet};
+// use near_sdk::json_types::U128;
+// use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{near_bindgen, AccountId};
+
+use crate::user::user::User;
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize)]
+pub struct BugBounty {
+    pub beneficiary: AccountId,
+    pub payments: UnorderedMap<AccountId, u128>,
+    accounts: UnorderedMap<AccountId, UnorderedSet<String>>,
+    users: LookupMap<AccountId, User>,
+    // bounties: LookupMap<String, Tournament>,
+    bounty_ids: UnorderedSet<String>,
+    // crowd_funded_bounties: LookupMap<String, Tournament>,
+    crowd_funded_bounties_ids: UnorderedSet<String>,
+}
+
+impl BugBounty {
+    pub fn create_user(&mut self, user: User) {
+        self.users.insert(&user.id, &user);
+    }
+}
--- contract/target/debug/deps/game_bloc-dbe09f97160a4484.d
+++ contract/target/debug/deps/game_bloc-dbe09f97160a4484.d
@@ -1,8 +1,11 @@
-/home/dunsin/gh/bugbounty/near_bug_bounty/contract/target/debug/deps/libgame_bloc-dbe09f97160a4484.rmeta: src/lib.rs src/crowd_funded_bounties.rs src/token_transfer.rs src/bounties.rs
+/home/dunsin/gh/bugbounty/near_bug_bounty/contract/target/debug/deps/libgame_bloc-dbe09f97160a4484.rmeta: src/lib.rs src/bounty/mod.rs src/bounty/bounty.rs src/database/mod.rs src/database/db.rs src/user/mod.rs src/user/user.rs
 
-/home/dunsin/gh/bugbounty/near_bug_bounty/contract/target/debug/deps/game_bloc-dbe09f97160a4484.d: src/lib.rs src/crowd_funded_bounties.rs src/token_transfer.rs src/bounties.rs
+/home/dunsin/gh/bugbounty/near_bug_bounty/contract/target/debug/deps/game_bloc-dbe09f97160a4484.d: src/lib.rs src/bounty/mod.rs src/bounty/bounty.rs src/database/mod.rs src/database/db.rs src/user/mod.rs src/user/user.rs
 
 src/lib.rs:
-src/crowd_funded_bounties.rs:
-src/token_transfer.rs:
-src/bounties.rs:
+src/bounty/mod.rs:
+src/bounty/bounty.rs:
+src/database/mod.rs:
+src/database/db.rs:
+src/user/mod.rs:
+src/user/user.rs:
