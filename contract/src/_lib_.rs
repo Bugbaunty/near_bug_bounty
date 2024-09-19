5ab0e72ea00cebe945716ce4dd04d701262dd1da
marvincd <mharvehll@gmail.com>
2024-09-19 23:25:05 +0100

    updated functions to use impl instead of refcell

--- contract/src/_lib_.rs
+++ contract/src/_lib_.rs
@@ -1,96 +1,63 @@
-use crate::tournaments::*;
-use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::{LookupMap, UnorderedMap, UnorderedSet};
-use near_sdk::json_types::U128;
-use near_sdk::serde::{Deserialize, Serialize};
-use near_sdk::Promise;
-use near_sdk::{env, log, near_bindgen, AccountId};
-
-mod crowd_funded_bounties;
-mod token_transfer;
-mod bounties;
-
-// const TOURNAMENT_NUMBER: u8 = 1;
-// // 5 Ⓝ in yoctoNEAR
-// const PRIZE_AMOUNT: U128 = near_sdk::json_types::U128(5_000_000_000_000_000_000_000_000);
-
-#[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
-pub struct BugBounty {
-    pub beneficiary: AccountId,
-    pub payments: UnorderedMap<AccountId, u128>,
-    accounts: UnorderedMap<AccountId, UnorderedSet<String>>,
-    users: LookupMap<AccountId, User>,
-    bounties: LookupMap<String, Tournament>,
-    bounty_ids: UnorderedSet<String>,
-    crowd_funded_bounties: LookupMap<String, Tournament>,
-    crowd_funded_bounties_ids: UnorderedSet<String>,
-}
-
-#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize, Debug)]
-#[serde(crate = "near_sdk::serde")]
-pub struct User {
-    user_id: AccountId,
-    age: u8,
-    status: Status,
-    // ⟵ Another struct we've defined
-    wins: u8,
-    username: String,
-}
-
-impl Default for BugBounty {
-    fn default() -> Self {
-        Self {
-            beneficiary: "v1.faucet.nonofficial.testnet".parse().unwrap(),
-            payments: UnorderedMap::new(b"d"),
-            accounts: UnorderedMap::new(b"t"),
-            bounties: LookupMap::new(b"c"),
-            users: LookupMap::new(b"c"),
-            bounty_ids: UnorderedSet::new(b"u"),
-            crowd_funded_bounties: LookupMap::new(b"c"),
-            crowd_funded_bounties_ids: UnorderedSet::new(b"u"),
-        }
-    }
-}
-
-#[near_bindgen]
-impl BugBounty {
-    #[init]
-    pub fn new() -> Self {
-        Self {
-            beneficiary: "v1.faucet.nonofficial.testnet".parse().unwrap(),
-            payments: UnorderedMap::new(b"d"),
-            accounts: UnorderedMap::new(b"t"),
-            bounties: LookupMap::new(b"c"),
-            users: LookupMap::new(b"c"),
-            bounty_ids: UnorderedSet::new(b"u"),
-            crowd_funded_bounties: LookupMap::new(b"c"),
-            crowd_funded_bounties_ids: UnorderedSet::new(b"u"),
-        }
-    }
-
-    // Public - beneficiary getter
-    pub fn get_beneficiary(&self) -> AccountId {
-        self.beneficiary.clone()
-    }
-
-    // Public - but only callable by env::current_account_id(). Sets the beneficiary
-    #[private]
-    pub fn change_beneficiary(&mut self, beneficiary: AccountId) {
-        self.beneficiary = beneficiary;
-    }
-
-    // pub fn get_all_users(&mut self) -> User {
-    //     let mut users = &self
-    //         .users;
-    //     users;
-    // }
-
-    pub fn get_user(&mut self, owner_id: AccountId) -> User {
-        let user = self
-            .users
-            .get(&owner_id)
-            .unwrap_or_else(|| env::panic_str("ERR_INCORRECT_USERID"));
-        return user;
-    }
-}
+// use near_sdk::test_utils::accounts;
+// use near_sdk::AccountId;
+// use std::cell::RefCell;
+//
+// pub use crate::database::db::BugBounty;
+// use crate::user::user::{RoleEnum, User};
+// use crate::bounty::*;
+// use crate::token_transfer::*;
+// use crate::user::*;
+//
+// mod bounty;
+// pub mod database;
+// mod user;
+//
+// thread_local! {
+//     static DB: RefCell<BugBounty> = RefCell::new(BugBounty::new(accounts(0)));
+// }
+//
+// pub fn create_user(
+//     id: AccountId,
+//     username: String,
+//     job: String,
+//     role: RoleEnum,
+//     location: String,
+//     skills: Vec<String>,
+// ) -> Result<(), String> {
+//     let user = User::new(id, username, job, role, location, skills);
+//     DB.with(|db| {
+//         let mut db = db.borrow_mut();
+//         let new_user = db.create_user(user);
+//         Ok(new_user)
+//     })
+// }
+//
+// pub fn get_user(id: AccountId) -> Result<Option<User>, String> {
+//     DB.with(|db| {
+//         let db = db.borrow_mut();
+//         let user = db.get_user(id);
+//         Ok(user)
+//     })
+// }
+//
+// pub fn edit_user(
+//     id: AccountId,
+//     username: Option<String>,
+//     job: Option<String>,
+//     verified: Option<bool>,
+//     role: Option<RoleEnum>,
+//     location: Option<String>,
+//     skills: Option<Vec<String>>,
+// ) {
+//     let user = User::edit(id, username, job, verified, role, location, skills);
+//     match user {
+//         Ok(user) => DB.with(|db| {
+//             let mut db = db.borrow_mut();
+//             db.edit_user(user)
+//         }),
+//         _ => {
+//             // Handle the error, for example by returning it
+//             // Err()
+//         }
+//     }
+// }
