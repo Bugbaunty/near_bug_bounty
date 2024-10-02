90e1e64bb2a49b1f8b5ef8ca1725aac3df5567f1
marvincd <mharvehll@gmail.com>
2024-10-02 12:10:52 +0100

    added all needed crud apis ommiting geting all structs

--- contract/src/bounties.rs
+++ contract/src/bounties.rs
@@ -37,6 +37,58 @@ pub_struct! ( BountyAccount {
      milestones: Option<Vec<MilestonesAccount>>,
 });
 
+pub_struct! ( BuildAccount {
+     id_hash: String,
+     creator: String,
+     creator_id: Option<String>,
+     status: BountyStatus,
+     idx: u8,
+     starting_date: String,
+     build_rules: String,
+     build_type: BountyType,
+     milestone: String,
+     guild:Vec<Guild>,
+     guild_points: Option<Vec<(String,u128)>>,
+     messages: Option<Vec<Chat>>,
+     user: Vec<String>,
+     winers: Vec<String>,
+     entry_prize: u8,
+     total_prize: u128,
+     no_of_winners: u8,
+     no_of_participants: u128,
+     milestone_type: MilestonesType,
+     end_date: String,
+     title: String,
+     points: Option<Vec<(String,u128)>>,
+     milestones: Option<Vec<MilestonesAccount>>,
+});
+
+pub_struct! ( BugAccount {
+     id_hash: String,
+     creator: String,
+     creator_id: Option<String>,
+     status: BountyStatus,
+     idx: u8,
+     starting_date: String,
+     bug_rules: String,
+     bug_type: BountyType,
+     milestone: String,
+     guild:Vec<Guild>,
+     guild_points: Option<Vec<(String,u128)>>,
+     messages: Option<Vec<Chat>>,
+     user: Vec<String>,
+     winers: Vec<String>,
+     entry_prize: u8,
+     total_prize: u128,
+     no_of_winners: u8,
+     no_of_participants: u128,
+     milestone_type: MilestonesType,
+     end_date: String,
+     title: String,
+     points: Option<Vec<(String,u128)>>,
+     milestones: Option<Vec<MilestonesAccount>>,
+});
+
 pub_struct! (  MilestonesAccount {
      status: BountyStatus,
      milestone_status: MilestonesStatus,
@@ -130,7 +182,105 @@ pub enum GuildType {
     Closed,
 }
 
+
+// beneficiary: "v1.faucet.nonofficial.testnet".parse().unwrap(),
+// payments: UnorderedMap::new(b"d"),
+// accounts: UnorderedMap::new(b"t"),
+// bounties: LookupMap::new(b"c"),
+// guilds: LookupMap::new(b"c"),
+// chats: LookupMap::new(b"c"),
+// builds: LookupMap::new(b"c"),
+// users: LookupMap::new(b"c"),
+// bounty_ids: UnorderedSet::new(b"u"),
+// bugs: LookupMap::new(b"c"),
+
 #[near_bindgen]
 impl BugBounty {
+    //bounties
+    pub fn insert_bounty(&mut self, bounty_id: String, value: BountyAccount) {
+        self.bounties.insert(&bounty_id, &value);
+    }
 
+    pub fn remove_bounty(&mut self, bounty_id: String,) {
+        self.bounties.remove(&bounty_id);
+    }
+
+    pub fn get_bounty(&self, bounty_id: String) -> Option<BountyAccount> {
+        self.bounties.get(&bounty_id)
+    }
+
+    pub fn does_bounty_exist(&self, bounty_id: String) -> bool {
+        self.bounties.contains_key(&bounty_id)
+    }
+
+
+    //guilds
+    pub fn insert_guild(&mut self, guild_id: String, value: Guild) {
+        self.guilds.insert(&guild_id, &value);
+    }
+
+    pub fn remove_guild(&mut self, guild_id: String,) {
+        self.guilds.remove(&guild_id);
+    }
+
+    pub fn get_guild(&self, guild_id: String) -> Option<Guild> {
+        self.guilds.get(&guild_id)
+    }
+
+    pub fn does_guild_exist(&self, guild_id: String) -> bool {
+        self.guilds.contains_key(&guild_id)
+    }
+
+    //chats
+    pub fn insert_chat(&mut self, chat_id: String, value: Chat) {
+        self.chats.insert(&chat_id, &value);
+    }
+
+    pub fn remove_chat(&mut self, chat_id: String,) {
+        self.chats.remove(&chat_id);
+    }
+
+    pub fn get_chat(&self, chat_id: String) -> Option<Chat> {
+        self.chats.get(&chat_id)
+    }
+
+    pub fn does_chat_exist(&self, chat_id: String) -> bool {
+        self.chats.contains_key(&chat_id)
+    }
+
+
+
+    //bugs
+    pub fn insert_bug(&mut self, bug_id: String, value: BugAccount) {
+        self.bugs.insert(&bug_id, &value);
+    }
+
+    pub fn remove_bug(&mut self, bug_id: String,) {
+        self.bugs.remove(&bug_id);
+    }
+
+    pub fn get_bug(&self, bug_id: String) -> Option<BugAccount> {
+        self.bugs.get(&bug_id)
+    }
+
+    pub fn does_chat_bug(&self, bug_id: String) -> bool {
+        self.bugs.contains_key(&bug_id)
+    }
+
+    //builds
+    pub fn insert_build(&mut self, build_id: String, value: BuildAccount) {
+        self.builds.insert(&build_id, &value);
+    }
+
+    pub fn remove_build(&mut self, build_id: String,) {
+        self.builds.remove(&build_id);
+    }
+
+    pub fn get_build(&self, build_id: String) -> Option<BuildAccount> {
+        self.builds.get(&build_id)
+    }
+
+    pub fn does_build_exist(&self, build_id: String) -> bool {
+        self.builds.contains_key(&build_id)
+    }
 }
--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -2,8 +2,10 @@ use crate::bounties::*;
 use crate::token_transfer::*;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::{LookupMap, UnorderedMap, UnorderedSet};
+// use near_sdk::store::{IterableMap, IterableSet, LookupMap, LookupSet, Vector};
 use near_sdk::json_types::U128;
 use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::BorshStorageKey;
 use near_sdk::Promise;
 use near_sdk::{env, log, near_bindgen, AccountId};
 
@@ -22,10 +24,14 @@ pub struct BugBounty {
     accounts: UnorderedMap<AccountId, UnorderedSet<String>>,
     users: LookupMap<AccountId, User>,
     bounties: LookupMap<String, bounties::BountyAccount>,
+    guilds: LookupMap<String, bounties::Guild>,
+    chats: LookupMap<String, bounties::Chat>,
+    builds: LookupMap<String, bounties::BuildAccount>,
+    bugs: LookupMap<String, bounties::BugAccount>,
     bounty_ids: UnorderedSet<String>,
 }
 
-#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize, Debug)]
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize, Debug, Clone)]
 #[serde(crate = "near_sdk::serde")]
 pub struct User {
     pub id_hash: String,
@@ -33,17 +39,17 @@ pub struct User {
     pub date: String,
     pub status: Status,
     pub bounties_wons: u8,
-    pub bountys_created:u8,
+    pub bountys_created: u8,
     pub points: Option<u128>,
     pub username: String,
     pub is_mod: bool,
     pub principal_id: String,
-    pub account_id : String,
+    pub account_id: String,
     pub canister_id: String,
     pub guild_badge: String,
 }
 
-#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize, Debug)]
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize, Debug, Clone)]
 #[serde(crate = "near_sdk::serde")]
 pub enum Status {
     Online,
@@ -57,8 +63,12 @@ impl Default for BugBounty {
             payments: UnorderedMap::new(b"d"),
             accounts: UnorderedMap::new(b"t"),
             bounties: LookupMap::new(b"c"),
+            guilds: LookupMap::new(b"c"),
+            chats: LookupMap::new(b"c"),
+            builds: LookupMap::new(b"c"),
             users: LookupMap::new(b"c"),
             bounty_ids: UnorderedSet::new(b"u"),
+            bugs: LookupMap::new(b"c"),
         }
     }
 }
@@ -72,8 +82,12 @@ impl BugBounty {
             payments: UnorderedMap::new(b"d"),
             accounts: UnorderedMap::new(b"t"),
             bounties: LookupMap::new(b"c"),
+            guilds: LookupMap::new(b"c"),
+            chats: LookupMap::new(b"c"),
+            builds: LookupMap::new(b"c"),
             users: LookupMap::new(b"c"),
             bounty_ids: UnorderedSet::new(b"u"),
+            bugs: LookupMap::new(b"c"),
         }
     }
 
@@ -82,23 +96,29 @@ impl BugBounty {
         self.beneficiary.clone()
     }
 
+    pub fn create_user(&mut self, account_id: AccountId, user: User) {
+        self.users.insert(&account_id, &user);
+    }
+
+    pub fn remove_user(&mut self, account_id: AccountId) {
+        self.users.remove(&account_id);
+    }
+
+    pub fn get_user(&self, account_id: AccountId) -> Option<User> {
+        self.users.get(&account_id)
+    }
+
+    pub fn is_user_present(&self, account_id: AccountId) -> bool {
+        self.users.contains_key(&account_id)
+    }
+
     // Public - but only callable by env::current_account_id(). Sets the beneficiary
     #[private]
     pub fn change_beneficiary(&mut self, beneficiary: AccountId) {
         self.beneficiary = beneficiary;
     }
 
-    // pub fn get_all_users(&mut self) -> User {
-    //     let mut users = &self
-    //         .users;
-    //     users;
+    // pub fn get_all_users(&self) -> LookupMap<AccountId, User> {
+    //     self.users.clone()
     // }
-
-    pub fn get_user(&mut self, owner_id: AccountId) -> User {
-        let user = self
-            .users
-            .get(&owner_id)
-            .unwrap_or_else(|| env::panic_str("ERR_INCORRECT_USERID"));
-        return user;
-    }
 }
