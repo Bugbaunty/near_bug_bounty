ad5e9c1dbc572bf233d37545f3d0ef787ee17aac
marvincd <mharvehll@gmail.com>
2024-10-27 23:18:24 +0100

    added tailwind config to frontend folder

---
+++ frontend/postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},  // Using Tailwind CSS as a string
+    autoprefixer: {},  // Using Autoprefixer as a string
+  },
+};
+// module.exports = {
+//   plugins: [
+//     require('tailwindcss'),
+//     require('autoprefixer'),
+//   ],
+// }
